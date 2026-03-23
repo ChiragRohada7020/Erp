ae2794f02cad40bd53dc45c6c6189791eaaabdc0
Chirag Rohada <chiragrohada40@gmail.com>
2026-03-23 11:55:09 +0530

    Initial project setup (Spring Boot + React + MySQL)

---
+++ Backend/erp-backend/src/main/java/com/erp/Test.java
@@ -0,0 +1,12 @@
+package com.erp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
